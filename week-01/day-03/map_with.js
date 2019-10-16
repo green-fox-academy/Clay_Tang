3d6cc455cc28d01e7a073756692195e97628cd4b
Clay Tang <Clay_Tang@epam.com>
2019-10-16 17:32:18 +0800

    casual commit

--- week-01/day-03/map_with.js
+++ week-01/day-03/map_with.js
@@ -6,6 +6,9 @@ const mapWith = (array, callback) => {
   // The mapWith() function should iterate over the given array and call the callback function on every element.
   // It stores the callback return values in the output.
   // The mapWith() should return with the output array.
+  array.forEach(i => {
+    output.push(callback(i));
+  })
 
   return output;
 }
@@ -19,11 +22,22 @@ const addOne = (number) => {
 console.log(mapWith([1, 2, 3], addOne));
 //expected result: [2, 3, 4]
 
+
 // Exercise 2:
 
 // Create a callback function which removes every second character from a string
 
 const words = ['map', 'reduce', 'filter'];
+
+const removeSecondLetter = (string) => {
+  for (let i = 1; i < string.length; i++){
+    if (i % 2 === 1){
+      string = string.replace(string[i], ' ');
+    }   
+  }
+
+  return string.split(' ').join('');
+}
 
 console.log(mapWith(words, removeSecondLetter));
 // expected result: ['mp','rdc', 'fle']
