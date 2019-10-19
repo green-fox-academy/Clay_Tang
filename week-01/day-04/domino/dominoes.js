edb4082469614884a9e4f4818387805d33eca821
Clay Tang <Clay_Tang@epam.com>
2019-10-19 10:17:15 +0800

    finish/modify majority of workshop, a few more to go

--- week-01/day-04/domino/dominoes.js
+++ week-01/day-04/domino/dominoes.js
@@ -19,12 +19,27 @@ const dominoes = initializeDominoes();
 /** eg: [2, 4], [4, 3], [3, 5] ... */
 
 
-// var new_dominoes = [];
+function check(domino1, domino2){
+  return domino1.values[1] === domino2.values[0];
+}
 
-// dominoes.forEach((values, index) => {
-//   var new_dominoes = [];
-  
-// })
+function rightOrder(dominoes){
+  for (let i = 0; i < dominoes.length - 1; i++) {
+    if (i === dominoes.length - 2) {
+      break;
+    }
+    if (check(dominoes[i], dominoes[i + 1])) {
+      continue;
+    }
+    for (let j = i + 2; j < dominoes.length; j++) {
+      if (check(dominoes[i], dominoes[j])) {
+        let temp = dominoes[i + 1];
+        dominoes[i + 1] = dominoes[j];
+        dominoes[j] = temp;
+      }
+    }
+  }
+}
 
-
+rightOrder(dominoes)
 console.log(dominoes.toString());
--- week-01/day-04/sharpie_set.js
+++ week-01/day-04/sharpie_set.js
@@ -5,28 +5,35 @@
 // countUsable() -> sharpie is usable if it has ink in it
 // removeTrash() -> removes all unusable sharpies
 
-// const Sharpie = require('./sharpie.js');
+const Sharpie = require('./sharpie.js');
 
-// class SharpieSet{
+class SharpieSet{
 
-//   constructor(){
-//     this.sharpie_set = [];
-//     this.sharpie_set.push(new Sharpie('black','1',50));
-//     this.sharpie_set.push(new Sharpie('blue','2',80));
-//     this.sharpie_set.push(new Sharpie('red','1',0));
-//     this.sharpie_set.push(new Sharpie('green','2',0));
-//   }
+  constructor(){
+    this.sharpie_set = [];
+    this.sharpie_set.push(new Sharpie('black','1',50));
+    this.sharpie_set.push(new Sharpie('blue','2',80));
+    this.sharpie_set.push(new Sharpie('red','1',0));
+    this.sharpie_set.push(new Sharpie('green','2',0));
+  }
 
-//   countUsable(){
-//     let count = 0;
-//     this.sharpie_set.forEach(sharpie => {
-//       if (sharpie.inkAmount)
-//     })
-//   }
+  countUsable(){
+    let amount = 0;
+		for (let i = 0; i < this.sharpie_set.length; i++) {
+			if (this.sharpie_set[i].inkAmount > 0) {
+				amount++;
+			}
+		}
+		return amount;
+  }
+  
+  removeTrash(){
+    this.list = this.list.filter((item) => {
+			return item.inkAmount > 0;
+		})
+  }
 
+}
 
-// }
-
-// var new_sharpies = new SharpieSet();
-
-// console.log(new_sharpies);
+var new_sharpies = new SharpieSet();
+console.log(new_sharpies.countUsable());
