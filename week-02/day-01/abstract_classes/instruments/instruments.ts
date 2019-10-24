6bf000de42f212fa0a4b375a5e2a2ddc56859e1a
Clay Tang <Clay_Tang@epam.com>
2019-10-24 14:50:24 +0800

    casual update

--- week-02/day-01/abstract_classes/instruments/instruments.ts
+++ week-02/day-01/abstract_classes/instruments/instruments.ts
@@ -8,15 +8,13 @@ abstract class Instrument {
 }
 
 abstract class StringedInstrument extends Instrument {
-  protected numberOfStrings: number;
+  public numberOfStrings: number;
 
   abstract sound();
 
   play() {
     console.log(
-      `${this.name}, a ${
-      this.numberOfStrings
-      }-stringed instrument that goes ${this.sound()}`
+      `${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`
     );
   }
 }
@@ -68,34 +66,3 @@ export class Violin extends StringedInstrument {
 // Bass Guitar (4 strings, 'Duum-duum-duum')
 // Violin (4 strings, 'Screech')
 // The Workshop should be invoked from another file like app.ts or main.ts
-console.log(
-  'Test 1, create Electric Guitar, Bass Guitar and Violin with default strings.'
-);
-const guitar = new ElectricGuitar();
-const bassGuitar = new BassGuitar();
-const violin = new Violin();
-
-console.log('Test 1 Play');
-guitar.play();
-bassGuitar.play();
-violin.play();
-
-console.log(
-  'Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings.'
-);
-const guitar2 = new ElectricGuitar(7);
-const bassGuitar2 = new BassGuitar(5);
-
-console.log('Test 2 Play');
-guitar2.play();
-bassGuitar2.play();
-// We expect the following output:
-
-// Test 1 Play
-// Electric Guitar, a 6-stringed instrument that goes Twang
-// Bass Guitar, a 4-stringed instrument that goes Duum-duum-duum
-// Violin, a 4-stringed instrument that goes Screech
-// Test 2, create Electric, Bass Guitar with 7 and 5 strings.
-// Test 2 Play
-// Electric Guitar, a 7-stringed instrument that goes Twangg
-// Bass Guitar, a 5-stringed instrument that goes Duum-duum-duum
---
+++ week-02/day-02/Arrow_function.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/* Write a function call in the commented area with an old anonym function and an arrow function. 
+Both anonym functions functions should take a number parameter and return it's square value. */
+
+function multiPurposeFunction(action) {
+  if (action) {
+    console.log(action(3));
+  }
+}
+
+function exampleNonAnonymFunction(param) {
+  return param * param;
+}
+
+function frameFunction() {
+  // this function call shows an example,
+  // but it is not using anonym functions
+  multiPurposeFunction(exampleNonAnonymFunction());
+  // write your code here
+}
+
+frameFunction();
+
+/* In the next exercise write two anonym functions again. This time the returned value is conditional.
+
+If the first parameter is an empty string, it should return the second parameter. 
+If not, it should return the two parameters joined by a single space. */
+
+
+function multiPurposeFunction(action) {
+  if (action) {
+    console.log(action('John', 'Smith'));
+    console.log(action('', 'Bond'));
+  }
+}
+
+function frameFunction() {
+  // Write your code here
+}
+
+frameFunction();
