7b5ad6a961c22cdc911c8369c4fb277e1e3e6e00
Clay Tang <Clay_Tang@epam.com>
2019-11-05 13:28:58 +0800

    commit my-app

---
+++ week-04/day-01/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import FirstComponent from './first-component';
+import SecondComponent from './second-component';
+
+
+let element = <SecondComponent items={[ 'a', 'b', 'c' ]} />;
+
+ReactDOM.render(element, document.getElementById('react-container'));
