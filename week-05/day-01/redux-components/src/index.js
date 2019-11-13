72c8360bd1a170eddb007556339ac08e7368c7c8
Clay Tang <Clay_Tang@epam.com>
2019-11-13 15:35:04 +0800

    rename the folder to redux-components

---
+++ week-05/day-01/redux-components/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import Reducer from './Reducer';
+
+const store = createStore(Reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
