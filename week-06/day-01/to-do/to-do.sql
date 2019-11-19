20fa94fb99d4c6c1a604df13a5128cde019f2160
Clay Tang <Clay_Tang@epam.com>
2019-11-19 11:07:18 +0800

    commit to-do

---
+++ week-06/day-01/to-do/to-do.sql
@@ -0,0 +1,26 @@
+create table todos
+(
+  taskName text,
+  done boolean
+)
+
+insert into todos
+values
+  ("Eat lunch", false),
+  ("Eat breakfast", true),
+  ("Go to work", true),
+  ("Eat dinner", false),
+  ("Sleep early", false);
+
+select *
+from todos;
+
+update todos
+set done = true 
+where taskName = "Eat lunch";
+
+select *
+from todos;
+
+delete from todos
+where taskName = "Sleep early";
